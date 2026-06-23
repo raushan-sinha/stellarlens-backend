ca7c015b36f43f1438845037510a4bc588a73d01
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-23 20:53:00 +0530

    Updated new caling server logic

--- server.js
+++ server.js
@@ -1,22 +1,7 @@
-import express from 'express';
-import cors from 'cors';
+import app from "./app.js";
 import dotenv from 'dotenv';
-import axios from 'axios';
 
-const app = express();   // Access Express power
-app.use(cors());   // Middleware - Connect Backend to Frontend
-app.use(express.json());
 dotenv.config();
-
-app.get('/api/space-data', async (req, res) => {
-    try {
-        const response = await axios.get(process.env.NASA_API);
-        res.status(200).json(response.data);
-    } catch (error) {
-        console.error('Error', error);
-        res.status(500).json({ message: 'Failed to fetch API' });
-    }
-});
 
 const Port = process.env.PORT || 5000;
 
