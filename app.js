0c28377529efef1dc288718856e7dbd9022fac80
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-23 20:51:30 +0530

    Added updated API calling logic

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import axios from 'axios';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// API call logic -
+app.get('/space-news', async (req, res) => {
+    try {
+        console.log('API called')
+        const response = await axios.get(process.env.NASA_API);
+        console.log(response.data)
+        return res.status(200).json(response.data);
+    } catch (error) {
+        console.error(error.response.data || error.message);
+        return res.status(500).json({ message: "Failed to fetch API" });
+    }
+});
+
+export default app;
